40aecfdbe6f1f71885d7dab9eb27dfddb6516fd6
samiatoui <atoui79@gmail.com>
2023-06-06 10:51:24 -0500

    test modified

--- src/__tests__/Server.test.js
+++ src/__tests__/Server.test.js
@@ -12,40 +12,22 @@ const server = createServer(yoga);
 const app = server.listen();
 
 describe("server", () => {
-  describe("/graphql", () => {
-    it("Returns 200", async () => {
-      const response = await request(app)
-        .post('/graphql')
-        .send({ query: '{ hello }' })
-      expect(response.status).toEqual(200)
-
-    })
-    describe("Hello Field", () => {
-      it("Returns Hello World", async () => {
-        const response = await request(app)
-          .post('/graphql')
-          .send({ query: '{ hello }' })
-        expect(response.body).toEqual({ data: { hello: "Hello World!" } })
-
-      })
-    })
     describe("Control Test", () => {
-      it("Returns Control", async () => {
-        const response = await request(app)
-          .post('/graphql')
-          .send({ query: ' {control(id: "AC-1"){title definition family id }}' })
-        expect(response.body).toEqual({
-          "data": {
-            "control": {
-              "title": "Access Control Policy and Procedures",
-              "definition": "(A)The organization develops, documents, and disseminates to [Assignment: organization-defined personnel or roles]:\n(a)An access control policy that addresses purpose, scope, roles, responsibilities, management commitment, coordination among organizational entities, and compliance; and\n(b)Procedures to facilitate the implementation of the access control policy and associated access controls.\n(B)The organization reviews and updates the current:\n(a)Access control policy [Assignment: organization-defined frequency]; and\n(b)Access control procedures [Assignment: organization-defined frequency].",
-              "family": "AC",
-              "id": "1"
-            }
-          }
+        it("Returns Control", async () => {
+            const response = await request(app)
+                .post('/graphql')
+                .send({ query: ' {control(id: "AC-1"){title definition family id }}' })
+            expect(response.body).toEqual({
+                "data": {
+                    "control": {
+                        "title": "Access Control Policy and Procedures",
+                        "definition": "(A)The organization develops, documents, and disseminates to [Assignment: organization-defined personnel or roles]:\n(a)An access control policy that addresses purpose, scope, roles, responsibilities, management commitment, coordination among organizational entities, and compliance; and\n(b)Procedures to facilitate the implementation of the access control policy and associated access controls.\n(B)The organization reviews and updates the current:\n(a)Access control policy [Assignment: organization-defined frequency]; and\n(b)Access control procedures [Assignment: organization-defined frequency].",
+                        "family": "AC",
+                        "id": "1"
+                    }
+                }
+            })
         })
-      })
     })
-  })
 })
 
