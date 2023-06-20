940caa663f1ab908365e699322370b7c943a70a4
samiatoui <atoui79@gmail.com>
2023-06-20 12:32:18 -0500

    buttons fixed

--- ui/src/Components/GetByDrop.js
+++ ui/src/Components/GetByDrop.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
 
 const GET_QUERY = gql`
-  query ($allocation: String!) {
+  query ControlDrop($allocation: String!) {
     controlDrop(allocation: $allocation) {
       control
       title
@@ -31,22 +31,15 @@ const GET_QUERY = gql`
 
 export default function GetByDrop() {
   const [allocation, setAllocation] = useState("");
-  const [getData, { loading, error, data }] = useLazyQuery(GET_QUERY);
-  const [activeButton, setActiveButton] = useState("");
+  const [getData, { loading, error, data }] = useLazyQuery(GET_QUERY, {
+    fetchPolicy: "cache-and-network",
+  });
 
   const handleClick = (event) => {
     event.preventDefault();
-    const clickedAllocation = event.target.value; // Get the value from the clicked button
-    setAllocation(clickedAllocation); // Set the allocation state with the clicked value
-
-    if (clickedAllocation) {
-      getData({ variables: { allocation: clickedAllocation } });
-    }
-
-    setActiveButton(clickedAllocation);
-
-    console.log(clickedAllocation)
-    console.log(activeButton)
+    const clickedAllocation = event.target.value;
+    setAllocation(clickedAllocation);
+    getData({ variables: { allocation: clickedAllocation } });
   };
 
   if (loading) return "Loading...";
@@ -58,150 +51,98 @@ export default function GetByDrop() {
     <div>
       <h2>Filter by Allocation</h2>
       <button
-        className={
-          activeButton === "department"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "department" ? "filter-button-clicked" : "filter-button"}
         value="department"
         onClick={handleClick}
       >
         Department
       </button>
       <button
-        className={
-          activeButton === "itSecurityFunction"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "itSecurityFunction" ? "filter-button-clicked" : "filter-button"}
         value="itSecurityFunction"
         onClick={handleClick}
       >
         IT Security Function
       </button>
       <button
-        className={
-          activeButton === "cioFunctionIncludingOps"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "cioFunctionIncludingOps" ? "filter-button-clicked" : "filter-button"}
         value="cioFunctionIncludingOps"
         onClick={handleClick}
       >
         CIO Function Including Ops
       </button>
       <button
-        className={
-          activeButton === "physicalSecurityGroup"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "physicalSecurityGroup" ? "filter-button-clicked" : "filter-button"}
         value="physicalSecurityGroup"
         onClick={handleClick}
       >
         Physical Security Group
       </button>
       <button
-        className={
-          activeButton === "personnelSecurityGroup"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "personnelSecurityGroup" ? "filter-button-clicked" : "filter-button"}
         value="personnelSecurityGroup"
         onClick={handleClick}
       >
         Personnel Security Group
       </button>
       <button
-        className={
-          activeButton === "programAndServiceDeliveryManagers"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "programAndServiceDeliveryManagers" ? "filter-button-clicked" : "filter-button"}
         value="programAndServiceDeliveryManagers"
         onClick={handleClick}
       >
         Program and Service Delivery Managers
       </button>
       <button
-        className={
-          activeButton === "process" ? "filter-button-clicked" : "filter-button"
-        }
+        className={allocation === "process" ? "filter-button-clicked" : "filter-button"}
         value="process"
         onClick={handleClick}
       >
         Process
       </button>
       <button
-        className={
-          activeButton === "project" ? "filter-button-clicked" : "filter-button"
-        }
+        className={allocation === "project" ? "filter-button-clicked" : "filter-button"}
         value="project"
         onClick={handleClick}
       >
-        CIO Function Including Ops
+        Project
       </button>
       <button
-        className={
-          activeButton === "itProjects"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "itProjects" ? "filter-button-clicked" : "filter-button"}
         value="itProjects"
         onClick={handleClick}
       >
         IT Projects
       </button>
       <button
-        className={
-          activeButton === "facilityAndHardware"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "facilityAndHardware" ? "filter-button-clicked" : "filter-button"}
         value="facilityAndHardware"
         onClick={handleClick}
       >
         Facility and Hardware
       </button>
       <button
-        className={
-          activeButton === "resourceAbstractionAndControlLayer"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "resourceAbstractionAndControlLayer" ? "filter-button-clicked" : "filter-button"}
         value="resourceAbstractionAndControlLayer"
         onClick={handleClick}
       >
         Resource Abstraction and Control Layer
       </button>
       <button
-        className={
-          activeButton === "infrastructure"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "infrastructure" ? "filter-button-clicked" : "filter-button"}
         value="infrastructure"
         onClick={handleClick}
       >
         Infrastructure
       </button>
       <button
-        className={
-          activeButton === "platform"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "platform" ? "filter-button-clicked" : "filter-button"}
         value="platform"
         onClick={handleClick}
       >
         Platform
       </button>
       <button
-        className={
-          activeButton === "application"
-            ? "filter-button-clicked"
-            : "filter-button"
-        }
+        className={allocation === "application" ? "filter-button-clicked" : "filter-button"}
         value="application"
         onClick={handleClick}
       >
