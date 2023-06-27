1c487e893b41f05bd9f8011df59a940b9ccf9bc3
samiatoui <atoui79@gmail.com>
2023-06-27 15:21:16 -0500

    env refactor

--- api/Index.js
+++ api/Index.js
@@ -3,8 +3,8 @@ import { Server } from "./src/Server.js";
 import { Database, aql } from "arangojs";
 import 'dotenv-safe/config.js'
 
-const { DB_URL, DB_NAME, DB_USER, DB_PASS } = process.env;
-
+// call the env variables
+const { DB_URL, DB_NAME, DB_USER, DB_PASS, PORT } = process.env;
 
 // actual db connection
 const db = new Database({
@@ -21,8 +21,8 @@ const query = async function query(strings, ...vars) {
 
 function index() {
   const server = Server({ schema, context: { query } });
-  server.listen(4000, () => {
-    console.info("Server is running on http://localhost:4000/graphql");
+  server.listen(PORT, () => {
+    console.info(`Server is running on http://localhost:${PORT}/graphql`);
   });
 }
 
