2c6e364ffda2039d506d855df1b01c73a829a7a5
Mike Williamson <mike@korora.ca>
2023-08-28 10:30:35 -0400

    Fix Apollo client error in App.test.js
    This commit adds Apollos MockedProvider component to the App test to
    resolve the error that was being produced.
    Components using Apollo client depend on the client to be available via
     the context.
    This client is used to fetch data from the API.
    When testing the MockedProvider is wrapped around components that
    need a client and test data is passed to the mocks prop allowing
    the MockedProvider to simulate an interaction with the API.
    As part of this refactor the queries are extracted into a separate
    file to allow imports into both the tests and the GetAll component.

--- ui/src/Components/GetAll.js
+++ ui/src/Components/GetAll.js
@@ -1,123 +1,61 @@
-import React, { useState } from "react";
-import { useQuery, gql } from "@apollo/client";
-
-const GET_ALL_CONTROLS = gql`
-  query {
-    controlAll {
-      control
-      title
-      definition
-      family
-      id
-      additionalGuidance
-      allocation {
-        department
-        itSecurityFunction
-        cioFunctionIncludingOps
-        physicalSecurityGroup
-        personnelSecurityGroup
-        programAndServiceDeliveryManagers
-        process
-        project
-        itProjects
-        facilityAndHardware
-        resourceAbstractionAndControlLayer
-        infrastructure
-        platform
-        application
-      }
-    }
-  }
-`;
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_ALL_CONTROLS } from '../graphql.js'
 
 export default function GetAllControls() {
-  const [keyword, setKeyword] = useState("");
-  const [allocation, setAllocation] = useState("");
-  const { loading, error, data } = useQuery(GET_ALL_CONTROLS);
+  const [keyword, setKeyword] = useState('')
+  const [allocation, setAllocation] = useState('')
+  const { loading, error, data } = useQuery(GET_ALL_CONTROLS)
 
   const handleChange = (event) => {
-    const newKeyword = event.target.value;
-    setKeyword(newKeyword);
-  };
-
-  const handleAllocationChange = (event) => {
-    const newAllocation = event.target.value;
-    setAllocation(newAllocation);
+    const newKeyword = event.target.value
+    setKeyword(newKeyword)
   }
 
-  if (loading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>;
+  const handleAllocationChange = (event) => {
+    const newAllocation = event.target.value
+    setAllocation(newAllocation)
+  }
+
+  if (loading) return 'Loading...'
+  if (error) return <pre>{error.message}</pre>
 
   const filteredControls = data.controlAll.filter((control) => {
     const isKeywordMatch =
-      control.control.toLowerCase().includes(keyword) ||
-      control.title.toLowerCase().includes(keyword);
+      control.control.toLowerCase().includes(keyword) || control.title.toLowerCase().includes(keyword)
 
-    if (allocation === "") {
-      return isKeywordMatch;
+    if (allocation === '') {
+      return isKeywordMatch
     }
 
-    const allocationValue = control.allocation[allocation];
-    return isKeywordMatch && allocationValue;
-  });
+    const allocationValue = control.allocation[allocation]
+    return isKeywordMatch && allocationValue
+  })
 
-  const numResults = filteredControls.length;
+  const numResults = filteredControls.length
 
   return (
     <div>
       <h2>Search by Keyword</h2>
 
       <div className="filter-options">
-        <input
-          type="text"
-          value={keyword}
-          onChange={handleChange}
-          placeholder="Search by keyword"
-        />
+        <input type="text" value={keyword} onChange={handleChange} placeholder="Search by keyword" />
         <select value={allocation} onChange={handleAllocationChange}>
           <option value="">All Allocations</option>
-          <option value="department">
-            Department
-          </option>
-          <option value="itSecurityFunction">
-            IT Security Function
-          </option>
-          <option value="cioFunctionIncludingOps">
-            CIO Function Including Ops
-          </option>
-          <option value="physicalSecurityGroup">
-            Physical Security Group
-          </option>
-          <option value="personnelSecurityGroup">
-            Personel Security Group
-          </option>
-          <option value="programAndServiceDeliveryManagers">
-            Program and Service Delivery Managers
-          </option>
-          <option value="process">
-            Process
-          </option>
-          <option value="project">
-            Project
-          </option>
-          <option value="itProjects">
-            IT Projects
-          </option>
-          <option value="facilityAndHardware">
-            Facility and Hardware
-          </option>
-          <option value="resourceAbstractionAndControlLayer">
-            Resource Abstraction and Control Layer
-          </option>
-          <option value="infrastructure">
-            Infrastructure
-          </option>
-          <option value="platform">
-            Platform
-          </option>
-          <option value="application">
-            Application
-          </option>
+          <option value="department">Department</option>
+          <option value="itSecurityFunction">IT Security Function</option>
+          <option value="cioFunctionIncludingOps">CIO Function Including Ops</option>
+          <option value="physicalSecurityGroup">Physical Security Group</option>
+          <option value="personnelSecurityGroup">Personel Security Group</option>
+          <option value="programAndServiceDeliveryManagers">Program and Service Delivery Managers</option>
+          <option value="process">Process</option>
+          <option value="project">Project</option>
+          <option value="itProjects">IT Projects</option>
+          <option value="facilityAndHardware">Facility and Hardware</option>
+          <option value="resourceAbstractionAndControlLayer">Resource Abstraction and Control Layer</option>
+          <option value="infrastructure">Infrastructure</option>
+          <option value="platform">Platform</option>
+          <option value="application">Application</option>
         </select>
       </div>
       <div>
@@ -141,7 +79,6 @@ export default function GetAllControls() {
                       ))}
                   </ul>
                 </div>
-
               </div>
             ))
           ) : (
@@ -150,5 +87,5 @@ export default function GetAllControls() {
         </div>
       </div>
     </div>
-  );
+  )
 }
