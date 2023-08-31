11f98eeb3d777fb7744a18e21e3ab7e621718b2f
Mike Williamson <mike@korora.ca>
2023-08-31 10:32:37 -0400

    Fix reference to renamed component
    This component was renamed in another commit and this reference was missed.

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,13 +3,13 @@ import GetData from "./Components/GetData.js";
 import GetByAllocation from "./Components/GetByAllocation";
 import GetAll from "./Components/GetAll";
 import { Wordmark } from "./Components/Wordmark.tsx";
-import { Phacsignature } from "./Components/PhacSig.tsx";
+import { PhacSignature } from "./Components/PhacSignature.js";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="header-sig">
-          <Phacsignature language="fr" />
+          <PhacSignature language="fr" />
         </div>
         <h1>DSCO ITSG33 Filter</h1>
       </header>
