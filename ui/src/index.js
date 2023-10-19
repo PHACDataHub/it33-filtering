085c2c2e70958984262ddf6c3553e4397178dce3
Stephen-ONeil <stephenlevioneil@gmail.com>
2023-10-19 15:44:05 -0400

    Realized there's no way to set a header on a preflight request, so you just can't make a cross origin request to a private GitHub codespaces forwarded port... then I realized create react app's dev server has had built in proxy support all along haha... ha

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,32 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 
-const get_apollo_http_link = () => {
-  if (process.env.REACT_APP_CODESPACE_NAME){
-    return createHttpLink({
-      uri: `https://${
-       process.env.REACT_APP_CODESPACE_NAME
-      }-${
-        process.env.REACT_APP_GQL_PORT
-      }.${
-        process.env.REACT_APP_CODESPACES_PORT_FORWARDING_DOMAIN
-      }/graphql `,
-      headers: {
-        "X-Github-Token": process.env.REACT_APP_CODESPACES_TOKEN
-      },
-    });
-  }
-  
-  return createHttpLink({
-    uri: `http://localhost:${process.env.REACT_APP_GQL_PORT}/graphql`,
-  });
-}
 
 const client = new ApolloClient({
-  link: get_apollo_http_link(),
+  uri: "/graphql", // for dev only, see ./setupProxy.js for proxying of this endpoint to the API
   cache: new InMemoryCache()
 });
 
